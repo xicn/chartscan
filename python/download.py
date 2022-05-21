c2e4eed62c582d9eb9019ded753074da6ac673e8
Xiaojie <okstrategie@gmail.com>
2022-05-21 22:54:02 +0000

    Python scripts

---
+++ python/download.py
@@ -0,0 +1,144 @@
+import os
+from os import path
+import bs4
+import threading
+import time
+import pandas
+import cloudscraper
+
+from regions import Regions
+
+
+def get_location(region):
+    return "/Users/xiaojiechen/Project/Parse/SpotifyData/" + region
+
+
+def range_date(start_date, end_date):
+    mydates = pandas.date_range(start_date, end_date).tolist()
+    list_1 = list()
+    for date in mydates:
+        list_1.append(pandas.Timestamp(date).date().strftime("%Y-%m-%d"))
+    return list_1
+
+
+def get_info(tag_obj, str, testing=False):
+    if str == "chart-table-track":
+        return get_track_artist(tag_obj, testing)
+
+    info = tag_obj["class"][0]
+    if info == str:
+        return tag_obj.text
+    else:
+        if testing is True:
+            return "This is not a " + str + " tag"
+
+
+def get_position(tag_obj, testing=False):
+    if tag_obj["class"][0] == "chart-table-position":
+        return "chart-table-position" + " is : " + tag_obj.text
+    else:
+        if testing is True:
+            return "This is not a " + "chart-table-position" + " tag"
+
+
+def get_track_artist(tag_obj, testing=False):
+    if tag_obj["class"][0] == "chart-table-track":
+        tag = list(tag_obj.children)
+        track_name = tag[1].text
+        artist = tag[3].text[3:]
+        return track_name, artist
+    else:
+        if testing is True:
+            return "This is not a " + "chart-table-track" + " tag"
+
+
+def get_streams(tag_obj, testing=False):
+    if tag_obj["class"][0] == "chart-table-streams":
+        return "chart-table-streams" + " is : " + tag_obj.text
+    else:
+        if testing is True:
+            return "This is not a " + "chart-table-streams" + " tag"
+
+# Get 201 rows that contains the tracks + header information
+
+
+def get_tracks(bs_obj, optional=False):
+    list_return = bs_obj.find_all("tr")
+    if optional is True:
+        return list(list_return)[1:]
+    return list(list_return)
+
+
+def write_file(out_put, list_tracks):
+    with open(out_put, "w") as f:
+        for track_tag in list_tracks:
+            pos = ""
+            stream = ""
+            track = ""
+            art = ""
+            for info in track_tag:
+                if isinstance(info, bs4.element.NavigableString):
+                    # print('yes it is instant of that')
+                    continue
+                elif isinstance(info, bs4.element.Tag):
+                    str = info["class"][0]
+                    if str == "chart-table-position":
+                        pos = get_info(info, str)
+                    elif str == "chart-table-track":
+                        if info is not None:
+                            (track, art) = get_track_artist(
+                                info)  # type: ignore
+                    elif str == "chart-table-streams":
+                        stream = get_info(info, str)
+            f.write("{}*{}*{}*{}\n".format(pos, track, art, stream))
+
+# This one also bypasses cloudflare
+
+
+def make_soup(region_code, date):
+    url_link = "https://spotifycharts.com/regional/" + region_code + "/daily/" + date
+    scraper = cloudscraper.create_scraper()
+    content = scraper.get(url_link).text
+    soup = bs4.BeautifulSoup(content, "html.parser")
+    return soup
+
+
+def get_download(date, region, region_code):
+    soup = make_soup(region_code, date)
+    lis = get_tracks(soup, True)
+    if len(lis) != 200:
+        return print("Error in html file", region, region_code, date)
+
+    location_dir = get_location(region)
+    if path.exists(location_dir) is False:
+        os.makedirs(location_dir)
+
+    out_put = location_dir + "/" + date + ".csv"
+    write_file(out_put, lis)
+    print("Successful")
+
+
+def createThread(date, region, region_code):
+    download_thread = threading.Thread(
+        target=get_download, args=(date, region, region_code)
+    )
+    download_thread.start()
+    return download_thread
+
+
+start_time = time.time()
+if __name__ == "__main__":
+    # get_download("2022-05-20", "Netherlands", "nl")
+    # get_download("2022-05-20", "Italy", "it")
+    # get_download("2022-05-20", "Philippines", "ph")
+
+    get_download("2022-05-20", "Thailand", "th")
+    get_download("2022-05-19", "Thailand", "th")
+    get_download("2022-05-18", "Thailand", "th")
+
+    # threads = []
+    # for countries in Regions:
+    #     threads.append(createThread("2022-05-20", countries.name, countries.value))
+
+    # for thread in threads:
+    #     thread.join()
---
+++ python/regions.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+
+class Regions(Enum):
+    Global = "global"
+    US = "us"
+    UK = "gb"
+    Australia = "au"
+    Canada = "ca"
+
+    France = "fr"
+    Netherlands = "nl"
+    Finland = "fi"
+    Norway = "no"
+    Denmark = "dk"
+    Sweden = "se"
+    Switzerland = "ch"
+    Ireland = "ie"
+    NewZealand = "nz"
+
+    Malaysia = "my"
+    Singapore = "sg"
+    Japan = "jp"
+    India = "in"
+    Philippines = "ph"
+    # Thailand = 'th'
+    Indonesia = "id"
+
+    Germany = "de"
+    Italy = "it"
+    Austria = "at"
+    Belgium = "be"
+    Hungary = "hu"
+
+    Portugal = "pt"
+    Spain = "es"
+    Brazil = "br"
+    Colombia = "co"
+    Mexico = "mx"
+    Argentina = "ar"
+    Poland = "pl"
+
+    Chile = "cl"
+    Turkey = "tr"
+    # SouthKorea = 'kr'
