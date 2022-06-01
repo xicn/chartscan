3e28ceb4c100ad894f3d589356105cee711aff41
Xiaojie <okstrategie@gmail.com>
2022-06-01 11:43:22 +0000

    Refactored spotify::fmt

--- src/spotify/fmt.rs
+++ src/spotify/fmt.rs
@@ -6,17 +6,13 @@ pub fn style1(gain: &SpotifyGain, region: String) {
     println!(
         "{:11} {:<21} {:3} {:3} [{:+4}] {:>10} {:>10} {:>10} {:>+5.2}%",
         region,
-        &gain.title[0..21],
+        gain.title,
         gain.yesterday_rank,
         gain.today_rank,
         gain.rank_diff,
-        gain.today_streams.to_formatted_string(&Locale::en),
-        gain.yesterday_streams.to_formatted_string(&Locale::en),
-        format!(
-            "[{}{}]",
-            if gain.streams_diff >= 0 { "+" } else { "" },
-            gain.streams_diff.to_formatted_string(&Locale::en)
-        ),
+        add_comma(gain.today_streams),
+        add_comma(gain.yesterday_streams),
+        add_comma_plus(gain.streams_diff),
         gain.percent_diff * 100f64
     );
 }
@@ -42,12 +38,51 @@ pub fn style2(gain: &SpotifyGain, region: String) {
     };
 
     if gain.percent_diff > 0.03 {
-        let str1 = format!("<tr>\n\t<td class=\"first\">{} [{:+}] {}</td>\n\t<td class=\"center\">{}</td>\n\t<td>{}</td>\n\t<td style=\"color:#1abc9c;\">{}</td>\n\t<td style=\"color:#1abc9c;\">{:+.2}%</td>\n</tr>\n",gain.today_rank, gain.rank_diff,symbol,  region, gain.today_streams.to_formatted_string(&Locale::en), format!("{}{}", if gain.streams_diff >= 0 { "+" } else { "" },
-        gain.streams_diff.to_formatted_string(&Locale::en)), gain.percent_diff * 100f64);
+        let str1 = get_html_str(
+            gain,
+            symbol,
+            &region,
+            Some(" style=\"color:#1abc9c;\"".to_string()),
+        );
         println!("{}", str1);
     } else {
-        let str1 = format!("<tr>\n\t<td class=\"first\">{} [{:+}] {}</td>\n\t<td class=\"center\">{}</td>\n\t<td>{}</td>\n\t<td>{}</td>\n\t<td>{:+.2}%</td>\n</tr>\n",gain.today_rank, gain.rank_diff,symbol,  region, gain.today_streams.to_formatted_string(&Locale::en), format!("{}{}", if gain.streams_diff >= 0 { "+" } else { "" },
-        gain.streams_diff.to_formatted_string(&Locale::en)), gain.percent_diff * 100f64);
+        let str1 = get_html_str(gain, symbol, &region, None);
+
         println!("{}", str1);
     }
+}
+
+fn get_html_str(gain: &SpotifyGain, symbol: &str, region: &str, style: Option<String>) -> String {
+    let style = style.unwrap_or("".to_string());
+    format!(
+        "\
+<tr>
+    <td class=\"first\">{} [{:+}] {}</td>
+    <td class=\"center\">{}</td>
+    <td>{}</td>
+    <td{}>{}</td>
+    <td{}>{:+.2}%</td>
+</tr>",
+        gain.today_rank,
+        gain.rank_diff,
+        symbol,
+        region,
+        add_comma(gain.today_streams),
+        style,
+        add_comma_plus(gain.streams_diff),
+        style,
+        gain.percent_diff * 100f64
+    )
+}
+
+fn add_comma(num: i64) -> String {
+    format!("{}", num.to_formatted_string(&Locale::en))
+}
+
+fn add_comma_plus(num: i64) -> String {
+    format!(
+        "{}{}",
+        if num >= 0 { "+" } else { "" },
+        num.to_formatted_string(&Locale::en)
+    )
 }
